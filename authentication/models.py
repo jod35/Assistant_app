cae00362c8d57d5bdb29f5fbae5e2e4e419d186b
Hans Ailo Siri <hansailosiri@gmail.com>
2022-07-11 16:46:25 +0200

    profile model and attempting to make user able to update data

--- authentication/models.py
+++ authentication/models.py
@@ -1,39 +1,58 @@
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from django.contrib.auth.models import (BaseUserManager, AbstractUser)
+
 
 # Manager for users
 class UserProfileManager(BaseUserManager):
-    def create_superuser(self, first_name, last_name, email, phone, date_of_birth, password):
-        if not email or not first_name or not last_name or not phone or not date_of_birth:
+    def create_superuser(self, first_name, last_name, email, phone, password):
+        if not email or not first_name or not last_name or not phone:
             raise ValueError('All fields are required')
 
         email = self.normalize_email(email)
-        user = self.model(first_name=first_name, last_name=last_name, phone=phone, email=email, date_of_birth=date_of_birth)
+        user = self.model(first_name=first_name, last_name=last_name, phone=phone, email=email)
         user.set_password(password)
         user.is_superuser = True
         user.is_staff = True
         user.save()
 
+        return user
+
+
+# Database model for users
 class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=80, unique=True)
+    email = models.EmailField(unique=True)
     phone = models.IntegerField()
     username = None
-    date_of_birth = models.DateField(null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    password = models.CharField(max_length=200)
+    updated_at = models.DateField(auto_now=True)
+    password = models.CharField(max_length=150)
 
     objects = UserProfileManager()
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', 'date_of_birth', 'password']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', 'password']
+    
 
     class Meta:
         verbose_name_plural = 'users'
         db_table = 'users'
 
     def __str__(self):
-        return str(self.username)
+        return self.email
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    alias_name = models.CharField(max_length=100)
+    profile_text = models.CharField(max_length=255)
+    profile_image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Profile'
+        db_table = 'Profile'
+
+    def __str__(self):
+        return self.email
--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,14 @@
+from urllib import response
 from django.contrib.auth import authenticate
 from django.shortcuts import render
+from fastapi import Depends
 from rest_framework import generics, status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 
-from .serializers import SignUpSerializer
+from .serializers import SignUpSerializer, UserSerializer
 from .tokens import create_jwt_pair_for_user
 
 
@@ -51,3 +54,31 @@ class LoginView(APIView):
         content = {"user": str(request.user), "auth": str(request.auth)}
 
         return Response(data=content, status=status.HTTP_200_OK)
+
+
+# Read user data
+@api_view(http_method_names=['GET'])
+def read_user(request:Request):
+    user = user.objects.filter(email=request.user)
+    serializer = UserSerializer(instance=user, many=True)
+
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+
+@api_view(http_method_names=['PUT'])
+def update_user(request=Request):
+    data = request.data
+
+    serializer = UserSerializer(instance=request.user, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    response = {
+        'message': 'User updated succsesfully',
+        'data': serializer.data
+    }
+
+
+    return Response(data=response, status=status.HTTP_200_OK)
