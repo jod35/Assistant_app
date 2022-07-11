84e0777d1003142b45e9998643afa730faa27678
Hans Ailo Siri <hansailosiri@gmail.com>
2022-07-11 17:24:49 +0200

    profile model and trying to make so user only can view his own data and update data

--- authentication/urls.py
+++ authentication/urls.py
@@ -10,6 +10,8 @@ from . import views
 urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("login/", views.LoginView.as_view(), name="login"),
+    path('read-user/', views.read_user, name='read-user'),
+    path('update-user/', views.update_user, name='update-user'),
     path("jwt/create/", TokenObtainPairView.as_view(), name="jwt_create"),
     path("jwt/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("jwt/verify/", TokenVerifyView.as_view(), name="token_verify"),
